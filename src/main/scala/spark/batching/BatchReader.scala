f4cc4cf18fcbc51afe9bef951076f4eff79265ad
Alelsandr <samsonovv.work@gmail.com>
2020-01-26 21:16:08 +0300

    removed kafka options from config

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -13,8 +13,6 @@ object BatchReader {
 
     val hotels_weather = ss.read.format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-      .option("kafka.value.deserializer", "spark.serdes.HotelWeatherDeserializer")
-      .option("kafka.key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
       .option("subscribe", "join-result-topic")
       .option("startingOffsets", "earliest")
       .option("endingOffsets", "latest")
@@ -25,9 +23,9 @@ object BatchReader {
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
 
+
     expedia.printSchema()
     hotels_weather.printSchema()
-
     ss.close()
   }
 }
