c90256465cc19817534032a5ba6c08df3ea1cb33
Alelsandr <samsonovv.work@gmail.com>
2020-01-27 15:39:24 +0300

    added expedia filtering, saving as avro in HDFS, count by hotel country and hotel city

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -3,6 +3,7 @@ package spark.batching
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import spark.data.HotelWeather
+
 object BatchReader {
 
   def main(args: Array[String]): Unit = {
@@ -12,7 +13,7 @@ object BatchReader {
       .master("yarn")
       .getOrCreate()
 
-    val df: DataFrame = ss.read.format("kafka")
+    val hotelsWeatherFromKafka: DataFrame = ss.read.format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
       .option("subscribe", "join-result-topic")
       .option("startingOffsets", "earliest")
@@ -21,21 +22,28 @@ object BatchReader {
 
     import ss.sqlContext.implicits._
 
-    val dataset = df.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
+    val hotelsWeather = hotelsWeatherFromKafka.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
 
     val expedia: DataFrame = ss
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
 
-    val joinExpediaHotelsWeather = dataset.join(expedia, dataset("hotelId") === expedia("hotel_id"), "inner")
+    val expediaWithIdleDays = expedia.withColumn("idleDays", datediff(expedia("srch_co"), expedia("srch_ci")))
 
-    val idleDays = expedia.filter(datediff(expedia("srch_co"), expedia("srch_ci")) >= 2 || datediff(expedia("srch_co"), expedia("srch_ci")) <= 30)
-    expedia.printSchema()
-    print(expedia.count())
-    print(idleDays.count())
-    df.printSchema()
-    joinExpediaHotelsWeather.printSchema()
-    idleDays.show(20)
+    val hotelsWeatherExpedia = hotelsWeather.join(expedia, hotelsWeather("hotelId") === expedia("hotel_id"), "inner")
+
+    val expediaFiltered = expedia.filter(datediff(expedia("srch_co"), expedia("srch_ci")) >= 2 || datediff(expedia("srch_co"), expedia("srch_ci")) <= 30)
+    expediaFiltered.write
+      .partitionBy("srch_ci")
+      .format("com.databricks.spark.avro")
+      .mode("overwrite")
+      .save("hdfs://sandbox-hdp.hortonworks.com:8020/tmp/expedia_batch_result/")
+
+    val countryCount = expediaFiltered.groupBy("user_location_country").count()
+    val cityCount = expediaFiltered.groupBy("user_location_city").count()
+    println("BOOKINGS BY HOTEL COUNTRY:" + countryCount.count())
+    println("BOOKINGS BY HOTEL CITY:" + cityCount.count())
+
     ss.close()
   }
 }
