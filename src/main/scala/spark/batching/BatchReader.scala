cc0c7d65e82264a6efb36ac09f2b1059c7333cf1
Alelsandr <samsonovv.work@gmail.com>
2020-01-27 13:23:40 +0300

    added filter for "invalid" data

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -1,8 +1,8 @@
 package spark.batching
 
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 import spark.data.HotelWeather
-
 object BatchReader {
 
   def main(args: Array[String]): Unit = {
@@ -17,21 +17,25 @@ object BatchReader {
       .option("subscribe", "join-result-topic")
       .option("startingOffsets", "earliest")
       .option("endingOffsets", "latest")
-      .option("maxOffsetsPerTrigger", "1")
       .load()
 
     import ss.sqlContext.implicits._
 
     val dataset = df.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
+
     val expedia: DataFrame = ss
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
 
-    val joinExpediaHotelsWeather = dataset.join(expedia,dataset("hotelId")===expedia("hotel_id"),"inner")
+    val joinExpediaHotelsWeather = dataset.join(expedia, dataset("hotelId") === expedia("hotel_id"), "inner")
 
+    val idleDays = expedia.filter(datediff(expedia("srch_co"), expedia("srch_ci")) >= 2 || datediff(expedia("srch_co"), expedia("srch_ci")) <= 30)
     expedia.printSchema()
+    print(expedia.count())
+    print(idleDays.count())
     df.printSchema()
     joinExpediaHotelsWeather.printSchema()
+    idleDays.show(20)
     ss.close()
   }
 }
