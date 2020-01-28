71ae6070e132ebea8fc2d4246430ca7ef044f8d5
Alelsandr <samsonovv.work@gmail.com>
2020-01-28 18:48:11 +0300

    added logger

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -1,17 +1,25 @@
 package spark.batching
 
+import com.typesafe.scalalogging._
+import org.slf4j.LoggerFactory
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import spark.data.HotelWeather
 
 object BatchReader {
 
+  private val logger = Logger(LoggerFactory.getLogger(this.getClass))
+
   def main(args: Array[String]): Unit = {
+
 
     val ss = SparkSession.builder()
       .appName("spark-batching-app")
       .master("yarn")
       .getOrCreate()
+    ss.sparkContext.setLogLevel("error")
+
+    logger.info("Start reading hotels from Kafka topic")
 
     val hotelsWeatherFromKafka: DataFrame = ss.read.format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
@@ -20,13 +28,19 @@ object BatchReader {
       .option("endingOffsets", "latest")
       .load()
 
+    if (hotelsWeatherFromKafka.limit(1).collect().isEmpty)
+      logger.info("Kafka topic is empty")
+    else
+      logger.info("HotelsWeather data from topic successfully read")
+
     import ss.sqlContext.implicits._
 
     val hotelsWeather = hotelsWeatherFromKafka.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
-
     val expedia: DataFrame = ss
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
+    if (expedia.count() > 0)
+      logger.info("Successfully read expedia data from HDFS")
 
     val expediaWithIdleDays = expedia.withColumn("idleDays", datediff(expedia("srch_co"), expedia("srch_ci")))
 
@@ -41,8 +55,8 @@ object BatchReader {
 
     val countryCount = expediaFiltered.groupBy("user_location_country").count()
     val cityCount = expediaFiltered.groupBy("user_location_city").count()
-    println("BOOKINGS BY HOTEL COUNTRY:" + countryCount.count())
-    println("BOOKINGS BY HOTEL CITY:" + cityCount.count())
+    logger.info("BOOKINGS BY HOTEL COUNTRY:" + countryCount.count())
+    logger.info("BOOKINGS BY HOTEL CITY:" + cityCount.count())
 
     ss.close()
   }
