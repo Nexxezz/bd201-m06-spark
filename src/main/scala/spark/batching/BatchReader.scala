52225c1c78897d1ce97bb6ed36a08265b96b1b7a
evgeny-rybnikov <evgeny.chaser@gmail.com>
2020-01-28 22:57:57 +0300

    minor refactoring

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -1,17 +1,15 @@
 package spark.batching
 
 import com.typesafe.scalalogging._
-import org.slf4j.LoggerFactory
-import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import spark.data.HotelWeather
 
 object BatchReader {
 
-  private val logger = Logger(LoggerFactory.getLogger(this.getClass))
+  private val LOG = Logger(getClass)
 
   def main(args: Array[String]): Unit = {
-
 
     val ss = SparkSession.builder()
       .appName("spark-batching-app")
@@ -19,7 +17,7 @@ object BatchReader {
       .getOrCreate()
     ss.sparkContext.setLogLevel("error")
 
-    logger.info("Start reading hotels from Kafka topic")
+    LOG.info("Start reading hotels from Kafka topic")
 
     val hotelsWeatherFromKafka: DataFrame = ss.read.format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
@@ -29,24 +27,27 @@ object BatchReader {
       .load()
 
     if (hotelsWeatherFromKafka.limit(1).collect().isEmpty)
-      logger.info("Kafka topic is empty")
+      LOG.info("Kafka topic is empty")
     else
-      logger.info("HotelsWeather data from topic successfully read")
+      LOG.info("HotelsWeather data from topic successfully read")
 
     import ss.sqlContext.implicits._
 
     val hotelsWeather = hotelsWeatherFromKafka.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
+
     val expedia: DataFrame = ss
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
+
     if (expedia.count() > 0)
-      logger.info("Successfully read expedia data from HDFS")
+      LOG.info("Successfully read expedia data from HDFS")
 
     val expediaWithIdleDays = expedia.withColumn("idleDays", datediff(expedia("srch_co"), expedia("srch_ci")))
 
     val hotelsWeatherExpedia = hotelsWeather.join(expedia, hotelsWeather("hotelId") === expedia("hotel_id"), "inner")
 
     val expediaFiltered = expedia.filter(datediff(expedia("srch_co"), expedia("srch_ci")) >= 2 || datediff(expedia("srch_co"), expedia("srch_ci")) <= 30)
+
     expediaFiltered.write
       .partitionBy("srch_ci")
       .format("com.databricks.spark.avro")
@@ -55,8 +56,9 @@ object BatchReader {
 
     val countryCount = expediaFiltered.groupBy("user_location_country").count()
     val cityCount = expediaFiltered.groupBy("user_location_city").count()
-    logger.info("BOOKINGS BY HOTEL COUNTRY:" + countryCount.count())
-    logger.info("BOOKINGS BY HOTEL CITY:" + cityCount.count())
+
+    LOG.info("BOOKINGS BY HOTEL COUNTRY:" + countryCount.count())
+    LOG.info("BOOKINGS BY HOTEL CITY:" + cityCount.count())
 
     ss.close()
   }
