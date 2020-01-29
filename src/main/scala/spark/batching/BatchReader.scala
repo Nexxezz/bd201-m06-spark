88b8ef3a875e2a2eef57d2ef2f80a38fa09dfbbd
Alelsandr <samsonovv.work@gmail.com>
2020-01-29 22:04:41 +0300

    addded screenshots

--- src/main/scala/spark/batching/BatchReader.scala
+++ src/main/scala/spark/batching/BatchReader.scala
@@ -19,6 +19,7 @@ object BatchReader {
 
     LOG.info("Start reading hotels from Kafka topic")
 
+    //Read hotels&weather data from Kafka with Spark application in a batch manner by specifying start offsets and batch limit in configuration file
     val hotelsWeatherFromKafka: DataFrame = ss.read.format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
       .option("subscribe", "join-result-topic")
@@ -35,6 +36,7 @@ object BatchReader {
 
     val hotelsWeather = hotelsWeatherFromKafka.map(row => HotelWeather.of(row.getAs[Array[Byte]]("value")))
 
+    //Read Expedia data from HDFS with Spark
     val expedia: DataFrame = ss
       .read.format("com.databricks.spark.avro")
       .load("/tmp/dataset/expedia")
@@ -42,18 +44,26 @@ object BatchReader {
     if (expedia.count() > 0)
       LOG.info("Successfully read expedia data from HDFS")
 
+    //Calculate idle days (days between current and previous check in dates) for every hotel.
     val expediaWithIdleDays = expedia.withColumn("idleDays", datediff(expedia("srch_co"), expedia("srch_ci")))
 
-    val hotelsWeatherExpedia = hotelsWeather.join(expedia, hotelsWeather("hotelId") === expedia("hotel_id"), "inner")
 
+
+    //Remove all booking data for hotels with at least one "invalid" row (with idle days more than or equal to 2 and less than 30)
     val expediaFiltered = expedia.filter(datediff(expedia("srch_co"), expedia("srch_ci")) >= 2 || datediff(expedia("srch_co"), expedia("srch_ci")) <= 30)
 
+    //Print hotels info (name, address, country etc) of "invalid" hotels and make a screenshot. Join expedia and hotel data for this purpose
+    val hotelsWeatherExpedia = hotelsWeather.join(expedia, hotelsWeather("hotelId") === expedia("hotel_id"), "inner")
+    hotelsWeatherExpedia.show(5)
+
+    //Store "valid" Expedia data in HDFS partitioned by year of "srch_ci"
     expediaFiltered.write
       .partitionBy("srch_ci")
       .format("com.databricks.spark.avro")
       .mode("overwrite")
       .save("hdfs://sandbox-hdp.hortonworks.com:8020/tmp/expedia_batch_result/")
 
+   // Group the remaining data and print bookings counts: 1) by hotel country, 2) by hotel city. Make screenshots of the outputs
     val countryCount = expediaFiltered.groupBy("user_location_country").count()
     val cityCount = expediaFiltered.groupBy("user_location_city").count()
 
